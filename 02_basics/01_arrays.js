1044e53313ca2a52f7eb9fe49945cd3aff539a41
Rohan Ambike <rambike@digitalzonein.local>
2025-01-10 22:05:11 +0530

    practicing

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,11 @@
+const myArr = [0, 1, 2, 3, 4];
+
+const fruits = ["let", "const", "var"];
+
+const identity = [339, "DZ0339", "Digitalzone"];
+
+const myArr2 = new Array(1, 2, 3, 4);
+
+
+myArr.push(6);
+console.log(myArr);
